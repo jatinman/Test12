c8eae34c30c4e16adaecd7a0368ec248b21c29ad
Jatin Maniar <jatin_maniar@dialog.com.au>
2019-11-26 04:03:13 +0000

    vso init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace workspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jatin";
+            Console.WriteLine($"Hello, {name}");
+        }
+    }
+}
